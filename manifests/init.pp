3f135d87dd80da1fe4f72e3ccc7b642a0776f00d
root <root@gateway-1.c.webforce3-378713.internal>
2023-03-09 12:24:24 +0000

    added some files

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+
+$conf_dir = $facts['os']['family'] ? {
+      'windows' => "${facts['common_appdata']}/PuppetLabs/puppet/etc",
+      default => '/etc/puppetlabs/puppet'
+  }
+
+  ini_setting { 'puppet agent: fact_value_length_soft_limit':
+      ensure => present,
+      path => "${conf_dir}/puppet.conf",
+      section => 'agent',
+      setting => 'fact_value_length_soft_limit',
+     value => '8192'
+  }
